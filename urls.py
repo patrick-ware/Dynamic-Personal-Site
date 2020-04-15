9eb243465bcb46521ef249873093acfca0b03267
Patrick Ware <patrick.r.ware@gmail.com>
2020-04-14 21:14:58 -0700

    minidjango files added to rep

--- urls.py
+++ urls.py
@@ -1,26 +1,16 @@
-"""homepage URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from patrickware import views
 
+import views
+
+# In this example, we've separated out the views.py into a new file
 urlpatterns = [
-    path('index/', views.index),
-    path('about-me/', views.about_me),
-    path('resume/', views.resume),
-    path('contact', views.contact),
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('about-me', views.about_me),
+    path('github-api-example', views.github_api_example),
 ]
+
+# Boilerplate to include static files
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- views.py
+++ views.py
@@ -1,22 +1,35 @@
+import requests
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-    context = {}
-    return render(request, 'base.html', context)
+    # This is similar to ones we have done before. Instead of keeping
+    # the HTML / template in a separate directory, we just reply with
+    # the HTML embedded here.
+    return HttpResponse('''
+        <h1>Welcome to my home page!</h1>
+        <a href="/about-me">About me</a> <br />
+        <a href="/github-api-example">See my GitHub contributions</a> <br />
+    ''')
 
 
 def about_me(request):
-    context = {}
-    return render(request, 'base.html', context)
+    # Django comes with a "shortcut" function called "render", that
+    # lets us read in HTML template files in separate directories to
+    # keep our code better organized.
+    context = {
+        'name': 'Ash Ketchum',
+        'pokemon': 'Pikachu',
+    }
+    return render(request, 'about_me.html', context)
 
 
-def resume(request):
-    context = {}
-    return render(request, 'base.html', context)
+def github_api_example(request):
+    # We can also combine Django with APIs
+    response = requests.get('https://api.github.com/users/janeqhacker/repos')
+    repos = response.json()
+    context = {
+        'github_repos': repos,
+    }
+    return render(request, 'github.html', context)
 
-
-def contact(request):
-    context = {}
-    return render(request, 'base.html', context)
