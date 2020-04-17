8a544c69c9329e84d72a24be95db31fc11b41ff5
Patrick Ware <patrick.r.ware@gmail.com>
2020-04-16 21:24:26 -0700

    github apit added to about me page, list of repos now displaying on page

--- views.py
+++ views.py
@@ -7,43 +7,34 @@ from django.shortcuts import render
 
 def index(request):
     print('index page requested...')
-    index_html = open('content/index.html').read()
-    context = {
-        'title' : 'index',
-        'view' : '100%',
-        'content' : index_html,
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'index.html', context)
 
 
 def about_me(request):
     print('about me page requested...')
-   # response = requests.get('https://api.github.com/users/patrick-ware/repos')
-   # repos = response.json()
-    context = {}
+    response = requests.get('https://api.github.com/users/patrick-ware/repos')
+    repos = response.json()
+    repo_list = [
+        repo['name']
+        for repo in repos
+    ]
+    context = {
+        'github_repos' : repo_list,
+    }
     return render(request, 'about_me.html', context)
 
 
 def resume(request):
     print('resume page requested...')
-    resume_html = open('content/resume.html').read()
-    context = {
-        'title' : 'resume',
-        'view' : '050%',
-        'content' : resume_html,
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'resume.html', context)
 
 
 def contact(request):
     print('contact page requested...')
-    contact_html = open('content/contact.html').read()
-    context = {
-        'title' : 'contact',
-        'view' : '100%',
-        'content' : contact_html,
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'contact.html', context)
 
 
 def github_api_example(request):
